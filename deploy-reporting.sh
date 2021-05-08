fbf470442723e427165649d691571184fa7c9ca2
codedoeseverything <codedoeseverything@gmail.com>
2021-05-09 00:16:57 +1000

    nine

--- deploy-reporting.sh
+++ deploy-reporting.sh
@@ -28,34 +28,30 @@ fi
 # 3. give permission to new file
 chmod +x ${GITHUB_REPOSITORY##*/}.csv
 
-ls -al
+# 4. perform variable value retrival. 
+REPO_URL=${GITHUB_REPOSITORY##*/}
+BRANCH=${GITHUB_REF##*/}
+COMMIT_SHA=${GITHUB_SHA:0:10}
+DATE=$(date +%D)
+TIME=$(date +%T)
+STATUSREPORT=$GITHUB_REPOSITORY,$BRANCH,$COMMIT_SHA,$GITHUB_ACTOR,$GITHUB_EVENT_NAME,$ENV,$AWS_ID,$REGION,$STATUS,$DATE,$TIME,$ENDPOINT
+PRACTERA_LOGO1='<section class="image-container">'
+PRACTERA_LOGO2='<img alt="Practera Logo" src="https://practera.com/wp-content/uploads/2020/11/Practera-logo-2.svg">'
+PRACTERA_LOGO3='</section>'
 
-cat ${GITHUB_REPOSITORY##*/}.csv
+# 5. update to csv file 
+sed -i -e "2i$STATUSREPORT" ${GITHUB_REPOSITORY##*/}.csv
 
-# # 4. perform variable value retrival. 
-# REPO_URL=${GITHUB_REPOSITORY##*/}
-# BRANCH=${GITHUB_REF##*/}
-# COMMIT_SHA=${GITHUB_SHA:0:10}
-# DATE=$(date +%D)
-# TIME=$(date +%T)
-# STATUSREPORT=$GITHUB_REPOSITORY,$BRANCH,$COMMIT_SHA,$GITHUB_ACTOR,$GITHUB_EVENT_NAME,$ENV,$AWS_ID,$REGION,$STATUS,$DATE,$TIME,$ENDPOINT
-# PRACTERA_LOGO1='<section class="image-container">'
-# PRACTERA_LOGO2='<img alt="Practera Logo" src="https://practera.com/wp-content/uploads/2020/11/Practera-logo-2.svg">'
-# PRACTERA_LOGO3='</section>'
+# 6. convert to html file 
+csvtotable ${GITHUB_REPOSITORY##*/}.csv ${GITHUB_REPOSITORY##*/}.html
 
-# # 5. update to csv file 
-# sed -i -e "2i$STATUSREPORT" ${GITHUB_REPOSITORY##*/}.csv
+# 7. check to modif html file as well for header part
+sed -i -e 's/\<Table\>/Practera DevOps Deployment Status/g' ${GITHUB_REPOSITORY##*/}.html
+sed -i -e "3i$PRACTERA_LOGO1" ${GITHUB_REPOSITORY##*/}.html
+sed -i -e "4i$PRACTERA_LOGO2" ${GITHUB_REPOSITORY##*/}.html
+sed -i -e "5i$PRACTERA_LOGO3" ${GITHUB_REPOSITORY##*/}.html
 
-# # 6. convert to html file 
-# csvtotable ${GITHUB_REPOSITORY##*/}.csv ${GITHUB_REPOSITORY##*/}.html
-
-# # 7. check to modif html file as well for header part
-# sed -i -e 's/\<Table\>/Practera DevOps Deployment Status/g' ${GITHUB_REPOSITORY##*/}.html
-# sed -i -e "3i$PRACTERA_LOGO1" ${GITHUB_REPOSITORY##*/}.html
-# sed -i -e "4i$PRACTERA_LOGO2" ${GITHUB_REPOSITORY##*/}.html
-# sed -i -e "5i$PRACTERA_LOGO3" ${GITHUB_REPOSITORY##*/}.html
-
-# # 8. copy back csv and html file
-# aws s3 cp ${GITHUB_REPOSITORY##*/}.csv s3://reporting123/${GITHUB_REPOSITORY##*/}.csv --acl public-read-write
-# aws s3 cp ${GITHUB_REPOSITORY##*/}.html s3://reporting123/${GITHUB_REPOSITORY##*/}.html --acl public-read-write
+# 8. copy back csv and html file
+aws s3 cp ${GITHUB_REPOSITORY##*/}.csv s3://reporting123/${GITHUB_REPOSITORY##*/}.csv --acl public-read-write
+aws s3 cp ${GITHUB_REPOSITORY##*/}.html s3://reporting123/${GITHUB_REPOSITORY##*/}.html --acl public-read-write
 
